d9c85f19822f9e4add4ad81cddf4c80b40fe4bcd
Maks Untilov <maksimuntilov7@gmail.com>
2023-03-22 21:04:45 +0200

    <Adding project>

---
+++ src/main/java/cinema/service/CustomUserDetailsService.java
@@ -0,0 +1,35 @@
+package cinema.service;
+
+import cinema.model.User;
+import java.util.Optional;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService
+        implements UserDetailsService {
+    private UserService userService;
+
+    public CustomUserDetailsService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> optionalUser = userService.findByEmail(username);
+        UserBuilder builder;
+        if (optionalUser.isPresent()) {
+            builder = org.springframework.security.core.userdetails.User.withUsername(username);
+            builder.password(optionalUser.get().getPassword());
+            builder.roles(optionalUser.get().getRoles()
+                    .stream()
+                    .map(role -> role.getRoleName().name())
+                    .toArray(String[]::new));
+            return builder.build();
+        }
+        throw new UsernameNotFoundException("UserNot found");
+    }
+}
